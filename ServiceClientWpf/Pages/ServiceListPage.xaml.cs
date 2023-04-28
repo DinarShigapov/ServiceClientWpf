17d0c0e21deb0caf317d22157a509aea5f740383
dinarhertz <dinarhertz@gmail.com>
2023-04-28 20:01:48 +0300

    ds

---
+++ ServiceClientWpf/Pages/ServiceListPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ServiceClientWpf.Model;
+
+namespace ServiceClientWpf.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ServiceListPage.xaml
+    /// </summary>
+    public partial class ServiceListPage : Page
+    {
+
+        public ServiceListPage()
+        {
+            InitializeComponent();
+            LVService.ItemsSource = App.DB.Service.ToList();
+        }
+
+        private void Refresh() 
+        {
+
+        }
+
+        private void CBSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void TBSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void CBFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+    }
+}
